353a931f1bf8aeb70b1efa24e7bcbb29f1ef5dad
JiaaiShen <jiaaishen@g.ucla.edu>
2021-10-23 15:56:45 -0700

    Create imdb_spider.py

---
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -0,0 +1,27 @@
+# to run
+# scrapy crawl imdb_spider -o movies.csv
+
+import scrapy
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb_spider'
+
+    start_urls = ['https://www.imdb.com/title/tt8962124/']
+
+    def parse(self, response):
+        """
+        """
+
+        next_page = response.urljoin('fullcredits/')
+
+        yield scrapy.Request(next_page, callback = self.parse_full_credits)
+
+
+    def parse_full_credits(self, response):
+        """
+        """
+
+        links = [a.attrib["href"] for a in response.css("td.primary_photo a")]
+
+        for link in links:
+            
